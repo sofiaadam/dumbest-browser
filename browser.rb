00ddcf0e74697fa774a6cbd8ee2a1391593ce511
Sofia AK <Marucat@MacBook-Air.local>
2017-04-10 17:12:55 +0800

    incomplete

--- browser.rb
+++ browser.rb
@@ -1,16 +1,25 @@
-require 'net/http'
-require 'nokogiri'
+# require 'net/http'
+# require 'nokogiri'
 
-require_relative 'util'
+require_relative 'page'
 
 class Browser
   def run!
     # Run the browser
     # Display a prompt for a user
+    puts "url> "
+    url = gets.chomp
+    puts "Fetching..."
     # Parse their input
     # Display useful results by instantiating
+    current_url = Page.new(url)
     #   a new Page and calling methods on it.
-    
+    current_url.fetch!
+    puts "Title: "
+    current_url.title
+    # puts "Content length: #{current_url} characters"
+    # puts "Links:"
+    # puts "#{current_url.links}"
     # Questions:
     #  1. How can a user quit the browser gracefully?
     #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
--- page.rb
+++ page.rb
@@ -1,14 +1,30 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+
 class Page
+  # attr_reader :title, :url, :links, :paragraph, :char_count
   def initialize(url)
+    @url = url
   end
   
   def fetch!
+    uri = URI(@url)
+    res = Net::HTTP.get_response(uri)
+    @doc = Nokogiri::HTML(res.body)
   end
   
   def title
+    p @doc.css('title').text
   end
   
   def links
+    @links = []
+    find_link = @doc.search("a").map { |tag| tag["href"]}
+    find_link.map! {|link|}
+    if link.start_with?"http://"
+      @link << link
+    end
     # Research alert!
     # How do you use Nokogiri to extract all the link URLs on a page?
     #
@@ -17,3 +33,6 @@ class Page
     # This would pull out "http://somesite.com/page.html"
   end
 end
+
+
+
